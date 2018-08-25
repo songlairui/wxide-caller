3d2d89b00d9d6e0547ff385c2e268f04d572bf80
songlr <songlr@mingyuanyun.com>
2018-08-25 16:16:29 +0800

    添加全局变量

--- utils/env.js
+++ utils/env.js
@@ -11,10 +11,27 @@ global.CLI = {
   isTestMode: false
 }
 global.appConfig = { isDev: false, isBeta: false, isGamma: false }
+
+const DIRECT = 'DIRECT'
+const proxyDict = {
+  'https://servicewechat.com': DIRECT
+}
+
 global.nw = {
   App: {
     getDataPath() {
       return 'C:\\Users\\songlr\\AppData\\Local\\微信web开发者工具\\User Data\\Default'
+    },
+    getProxyForURL(...x) {
+      return (
+        proxyDict[x[0]] ||
+          console.warn('\n--------------------------need proxy for--', x),
+        DIRECT
+      )
     }
   }
 }
+
+Object.assign(global, {
+  appVersion: '1.02.1808101'
+})
