6c1bc08f7b24f3102af35cf5cdc93da647c20f09
songlr <songlr@mingyuanyun.com>
2018-08-30 03:02:48 +0800

    查看占用进程的变量

--- playground/login/test.js
+++ playground/login/test.js
@@ -1,5 +1,20 @@
 const test = require('./f-f2b0')
 
 const a = test.reloadQRCode()
-
+console.warn(typeof a)
 a.then ? a.then(console.warn) : console.warn(a)
+console.warn('----------------===-------------')
+
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read()
+  if (chunk !== null) {
+    process.stdout.write(`data: ${chunk}`)
+
+    const a = process._getActiveRequests(),
+      b = process._getActiveHandles()
+    console.warn(a.length, b.length)
+    if (chunk.toString() === 'a') {
+      console.warn({ a, b })
+    }
+  }
+})
